728726c61bb625c507d70cea2a5f2c50f054bf88
Utsab Adhikari <uadhikari@xylontech.com>
2024-03-21 16:57:29 +0545

    changes dotenv files

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
 const port = process.env.port || 4000;
+require("dotenv").config();
+
 const checkToken = (req, res, next) => {
   if (req.headers.token !== "bearer token123") {
     res.send(400, "Invalid acess token");
